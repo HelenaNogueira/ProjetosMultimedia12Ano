c1586e929f65dbedccbfea30c2da26a0ccde08c9
HelenaNogueira <166917371+HelenaNogueira@users.noreply.github.com>
2026-05-20 11:15:29 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -3,11 +3,12 @@
 // 3) Project settings > General > Your apps > Web app
 // 4) Copia a configuração para aqui.
 
-export const firebaseConfig = {
-  apiKey: "COLOCA_AQUI",
-  authDomain: "COLOCA_AQUI.firebaseapp.com",
-  projectId: "COLOCA_AQUI",
-  storageBucket: "COLOCA_AQUI.firebasestorage.app",
-  messagingSenderId: "COLOCA_AQUI",
-  appId: "COLOCA_AQUI"
+const firebaseConfig = {
+  apiKey: "AIzaSyC8bLqmqGHvNTXt0jE6N5zqjLI7GV0t9iQ",
+  authDomain: "projetosmultim.firebaseapp.com",
+  projectId: "projetosmultim",
+  storageBucket: "projetosmultim.firebasestorage.app",
+  messagingSenderId: "629182215720",
+  appId: "1:629182215720:web:e6d1ce4d3cb8aadac4a71d",
+  measurementId: "G-X4XVHPJLHW"
 };
