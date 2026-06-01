71ab8d955215a8189bec2e8f46a9c27cef85d418
HelenaNogueira <166917371+HelenaNogueira@users.noreply.github.com>
2026-06-01 10:44:10 +0100

    Change productUrl for 'Cinco Minutos de Futuro'
    Updated productUrl for 'Cinco Minutos de Futuro'.

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ const projects = [
   { turma: '12.ºF', nome: 'Infocraft', productUrl: '', videoUrl: '' },
   { turma: '12.ºF', nome: 'Skyscratcher', productUrl: '', videoUrl: '' },
   { turma: '12.ºF', nome: 'Voltsun', productUrl: '', videoUrl: '' },
-  { turma: '12.ºH', nome: 'Cinco Minutos de Futuro', productUrl: '', videoUrl: 'https://youtu.be/daodKNe3tDI' },
+  { turma: '12.ºH', nome: 'Cinco Minutos de Futuro', productUrl: 'https://youtu.be/T-WNHQ6AQGc', videoUrl: 'https://youtu.be/daodKNe3tDI' },
   { turma: '12.ºH', nome: 'BIG6', productUrl: '', videoUrl: 'https://youtu.be/1uHiAzdDNW0' },
   { turma: '12.ºH', nome: 'Bugados mas Informados', productUrl: '', videoUrl: 'https://youtu.be/Jj3_bTXnsNw' },
   { turma: '12.ºH', nome: 'Entre Linhas', productUrl: '', videoUrl: 'https://youtu.be/_VYet6RjFg4' }
